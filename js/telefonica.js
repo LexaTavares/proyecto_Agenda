3bebb2069f96b69dcaba0659d70fba3995e73860
LexaTavares <alexis.tavares.chavez@gmail.com>
2017-06-09 17:36:55 -0500

    lista telefonica en html

--- js/telefonica.js
+++ js/telefonica.js
@@ -1,25 +1,48 @@
-var ingresarDatos= function(){
-  var nombre=prompt("nombre");
-  var apellido=prompt("Apellido");
-  var telefonico=prompt("Numero");
-  var seccionLista=$("#seccionLista");
+var agenda= [
+  {
+    "nombre": "Emiliano Garcia",
+    "numero": "54789612",
+  },
+  {
+    "nombre": "Pepe Perez",
+    "numero": "25478532",
+  },
+  {
+    "nombre": "Israel Gonzalez",
+    "numero": "56479852",
+  },
+  {
+    "nombre": "Noehmi Blancas",
+    "numero": "56982157",
+  },
+  {
+    "nombre": "Camila Lopez",
+    "numero": "53487914",
+  },
+  {
+    "nombre": "Maximiliano Cortez",
+    "numero": "58421765",
+  },
+];
 
-  seccionLista.text
+var listaH=
+'<div>'+
+  '<li>__nombre__</li>'+
+  '<p>__numero__</p>'+
+'</div>';
 
+var cargarPagina=function(){
+ mostrar(agenda);
+};
 
-}
+var mostrar=function(agenda){
+ var vacio="";
 
+ agenda.forEach(function(agregar){
+   vacio+=listaH.replace("__nombre__", agregar.nombre)
+   .replace("__numero__", agregar.numero);
+ });
+ $(".seccionLista").html(vacio);
+};
 
-$(document).ready(function(){
-var botonContacto=$("#contacto");
-
-
-  botonContacto.click(ingresarDatos);
-
-
-
-
-
-
-
-})
+$(document).ready(cargarPagina);
