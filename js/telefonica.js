fd841191eac49dcd1a38d864af679d782d1ac199
LexaTavares <alexis.tavares.chavez@gmail.com>
2017-06-09 16:47:28 -0500

    lista telefonica

---
+++ js/telefonica.js
@@ -0,0 +1,25 @@
+var ingresarDatos= function(){
+  var nombre=prompt("nombre");
+  var apellido=prompt("Apellido");
+  var telefonico=prompt("Numero");
+  var seccionLista=$("#seccionLista");
+
+  seccionLista.text
+
+
+}
+
+
+$(document).ready(function(){
+var botonContacto=$("#contacto");
+
+
+  botonContacto.click(ingresarDatos);
+
+
+
+
+
+
+
+})
